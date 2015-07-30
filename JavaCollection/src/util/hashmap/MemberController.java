7d2fad301a3a050851b6abf365be45d3196c4012
itbank <itbank@itbank-PC>
2015-07-30 15:17:14 +0900

    2015.07.30 해쉬맵 마무리 쓰레드

--- JavaCollection/src/util/hashmap/MemberController.java
+++ JavaCollection/src/util/hashmap/MemberController.java
@@ -4,11 +4,14 @@ import java.util.Scanner;
 
 public class MemberController {
 	public static void main(String[] args) {
-		MemberService m = new MemberServiceImpl();
-		Member hong = new Member("qwe","123","홍길동",12,"서울");
-		m.create("qwe", hong);
+		MemberService memberService  = new MemberServiceImpl();
+		
+		/*Member hong = new Member("qwe","123","홍길동",12,"서울");
+		memberService.create("qwe", hong);*/
+		
 		Scanner scanner = new Scanner(System.in);
 		String input = "", key =""; 
+		
 		while (true) {
 			System.out.println("1.[JOIN]회원가입 2.[LOGIN]로그인 0.[LOGOUT]로그아웃");
 			 input = scanner.next();
@@ -30,14 +33,14 @@ public class MemberController {
 					System.out.println("주소를 입력하세요");
 					String adr = scanner.next();
 					Member member = new Member(id,pass,name,age,adr);
-					 m.create(id, member);
+					memberService.create(id, member);
 					break;
 				case "LOGIN":
 					System.out.println("아이디를 입력하세요");
 					String id1 = scanner.next();
 					System.out.println("비밀번호 입력하세요");
 					String pass1 = scanner.next();
-					String result =m.login(id1, pass1);
+					String result =memberService.login(id1, pass1);
 					System.out.println(id1+"님");
 					
 					break;
--- JavaCollection/src/util/hashmap/MemberServiceImpl.java
+++ JavaCollection/src/util/hashmap/MemberServiceImpl.java
@@ -1,7 +1,9 @@
 package util.hashmap;
 
 import java.util.HashMap;
+import java.util.Iterator;
 import java.util.Map;
+import java.util.Set;
 
 public class MemberServiceImpl implements MemberService {
 	Map<String, Member> map = new HashMap<String, Member>();
@@ -11,10 +13,24 @@ public class MemberServiceImpl implements MemberService {
 	public void create(String id,Member member) {
 		
 		map.put(id, member);
+		
 	}
 
 	@Override
 	public String login(String id1,String pass1) {
+		Set set = map.entrySet();
+		//Iterator는 순환기 라는 의미 =>지하철2호선 
+		Iterator it = set.iterator();
+		//it.hasNext() 은 밸류(값)가 커서 (이터레이터)다음에 있는지 없는지를 따저서 있으면
+		//while 룹을 계속 돌리고 없으면 스톱한다.
+		/*
+		 1.키와 밸류를 동시에 출력 
+		 * */
+		while (it.hasNext()) {
+			Map.Entry e = (Map.Entry) it.next();
+	        System.out.println("ID :" + e.getKey() + ", 회원객체" + e.getValue());
+	/*		
+		}
 		String msg = "";
 			
 			String loginid = map.get("id").getId();
@@ -24,10 +40,10 @@ public class MemberServiceImpl implements MemberService {
 				 
 			}else{
 				msg = "잘못된 아이디 이거나 비밀번호 입니다.";
-			}
+			}*/
 			
 		
-		return msg;
+		return  
 	}
 	
 }
